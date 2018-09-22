4ffd40e8f46fb1e9a7365fdc6015b61da3e8cf60
alec <abenson@wpi.edu>
2018-09-22 11:52:14 -0400

    features added

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 
 const express = require('express');
 const childProcess = require('child_process');
-const nodeimu = require('nodeimu');
+const BB = require('bluebird');
+const nodeimu = require('node-sense-hat').Imu;
+const GPIO = require('onoff').Gpio;
 
 
-const PORT = 5000;
+
+const PORT = 6000;
+const PIN = 21;
+const HIGH = 1;
+const LOW = 0;
+
+
 let IMU = new nodeimu.IMU();
+let light = new GPIO(PIN, 'out');
 let app = express();
 
 function cToF(tempC) {
@@ -14,18 +23,92 @@ function cToF(tempC) {
 }
 
 function getCPUTemp() {
-	return new Promise(resolve => {
+	return new BB(resolve => {
 		let temp = childProcess.spawn('cat', ['/sys/class/thermal/thermal_zone0/temp']);
 		temp.stdout.on('data', data => {
-			let tempC = data / 1000;
-			resolve(cToF(tempC));
+			resolve(data / 1000);
 		});
 	});
 }
 
+function getIMUData() {
+	return new BB((resolve, reject) => {
+		IMU.getValue((err, data) => {
+			if(err !== null) {
+				console.error(err);
+				return reject(err);
+			}
+			resolve(data);
+		});
+	});
+}
+
+function lightsOn() {
+	return light.writeSync(HIGH);
+}
+
+function lightsOff() {
+	return light.writeSync(LOW);
+}
+
+function lightsToggle() {
+	if(light.readSync() === LOW) {
+		return lightsOn();
+	} else {
+		return lightsOff();
+	}
+}
+
+function getIMUTemp() {
+	return getIMUData().then(res => res.temperature);
+}
+
+function getIMUHumidity() {
+	return getIMUData().then(res => res.humidity);
+}
+
+function getAmbientHumidity() {
+	return getIMUHumidity();
+}
+
+function getAmbientTemp() {
+	return BB.all([getIMUTemp(), getCPUTemp()]).spread((ambientTemp, cpuTemp) => {
+		let modifier = (cpuTemp - ambientTemp) / 1.886;
+		let tempCalibrated = ambientTemp - modifier;
+		return cToF(tempCalibrated);
+	});
+}
+
+app.get('/temperature', (req, res) => {
+	getAmbientTemp().then(temp => res.json(temp));
+});
+
+app.get('/humidity', (req, res) => {
+	getAmbientHumidity().then(hum => res.json(hum));
+});
+
+app.get('/on', (req, res) => {
+	lightsOn();
+	res.json('ON');
+});
+
+app.get('/off', (req, res) => {
+	lightsOff();
+	res.json('OFF');
+});
+
+app.get('/toggle', (req, res) => {
+	lightsToggle();
+	light.readSync() === 0
+		? res.json('OFF')
+		: res.json('ON');
+});
 
 
-getCPUTemp().then(console.log);
+
+setTimeout(() => {
+	process.exit(1);
+},60000);
 
 
-app.listen(PORT);
+app.listen(PORT, '127.0.0.1');
